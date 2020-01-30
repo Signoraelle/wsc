1563682f173e5a0c6f64f0ff9a6abcb41df9700d
Signoraelle <59834432+Signoraelle@users.noreply.github.com>
2020-01-30 09:44:52 +0100

    javascript
    4 first exercise odyssey

---
+++ abs.js
@@ -0,0 +1,34 @@
+"use strict";
+
+/*
+ * Create the `abs` function that takes one number argument
+ * and returns its absolute value.
+ * You are not allowed to use `Math.abs`, make your own.
+ * see: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/abs
+ *
+ * @notions Functions, Ternary
+ */
+
+// Your code:
+const abs = n => {
+  if (n < 0) {
+    return n * -1;
+  } else {
+    return n;
+  }
+};
+
+const absTernary = n => (n < 0 ? n * -1 : n);
+
+//* Begin of tests
+const assert = require("assert");
+
+assert.strictEqual(typeof abs, "function");
+assert.strictEqual(abs.toString().includes("Math.abs"), false);
+assert.notStrictEqual(abs, Math.abs);
+assert.strictEqual(abs.length, 1);
+assert.strictEqual(abs(0), 0);
+assert.strictEqual(abs(-1), 1);
+assert.strictEqual(abs(-13.2), 13.2);
+assert.strictEqual(abs(132), 132);
+// End of tests */
